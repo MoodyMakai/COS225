8c563d8bc5628d82ec900a9cc961be1c023f2b67
Makai Moody-Broen <69447009+MoodyMakai@users.noreply.github.com>
2024-01-25 13:46:08 -0500

    Hw_0

---
+++ HW_0.java
@@ -0,0 +1,6 @@
+public class HW_0 {
+    public static void main(String[] args){
+        System.out.println("hello World!\nMakai Moody-Broen");
+    
+    }
+}
---
+++ Week1.java
@@ -0,0 +1,59 @@
+public class Week1 {
+    //Types: Types cannot be infered by editor, 
+    //type must be provided with assignment,
+    //Definition, Decleration, and initialization are separated steps in Java
+    //
+    //Decleration: specification of data Type
+    //
+    //Initialization: Specification of data value
+    //
+    // Ex:: int x = 5, double x = 5.5
+    //
+    //Java uses staticlly typed primitives, types are a promise
+    //This means Variable types cannot be changed
+    //
+    //Java Primitives:
+    //Int, Float, Double, Long, Short, Byte, Char, Boolean
+    //
+    //Non-Primitves:
+    //String, Array, Losts, Files< every other object
+    //
+    //SemiColon required for line break
+    //
+    //Functions:
+    //Type must be provided for functions,cmost components follow the same pattern as python
+    //Java Function EX:
+
+    public int HelloWorld(){
+        System.out.println("Hello World");
+        //println creates new line
+        System.out.print("no new line");
+        return 5;
+    }
+
+    //in Java, visibilty modifiers must be provided for functions
+    //In this case the modifier is the word "Public"
+    //Public, Private, and Protected all exist
+    //expected return type must be provided 
+    //essentially this gives types to functions
+    //
+    //Curly braces must be used to specify functions, indents do not matter
+    //Function call: 
+        // HelloWorld();
+    // System Library, Output stream, and print function must all be referenced to print, hence the System.out.println
+    //
+    //Java if statement:
+    // if (boolean){ 
+        //task
+    //}
+    // else if (boolean) {
+        //stuff
+    //}
+    //
+    //Java For loop
+    // for (int i = 0; i<10; i ++){
+        //repeat 10 times
+    //}
+    }
+
+
