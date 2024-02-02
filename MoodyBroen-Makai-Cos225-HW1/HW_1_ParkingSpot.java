1c7b8532856c8dbc12e08c324b6302047196f355
Makai Moody-Broen <69447009+MoodyMakai@users.noreply.github.com>
2024-02-02 11:06:58 -0500

    Delete MoodyBroen-Makai-Cos225-HW1 directory

--- MoodyBroen-Makai-Cos225-HW1/HW_1_ParkingSpot.java
+++
@@ -1,9 +0,0 @@
-public class HW_1_ParkingSpot {
-    public HW_1_Car parkedCar;
-    public boolean handicap;
-
-    public HW_1_ParkingSpot (boolean handicap){
-        this.handicap = handicap;
-    }
-
-}
